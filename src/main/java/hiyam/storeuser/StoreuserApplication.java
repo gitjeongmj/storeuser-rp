927f5820c461d8da2b9189eed9f1818719f652a1
gitjeongmj <mg4098@naver.com>
2025-03-10 21:28:42 +0900

    초기 프로젝트 설정

---
+++ src/main/java/hiyam/storeuser/StoreuserApplication.java
@@ -0,0 +1,13 @@
+package hiyam.storeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreuserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hiyam/storeuser/StoreuserApplicationTests.java
@@ -0,0 +1,13 @@
+package hiyam.storeuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
